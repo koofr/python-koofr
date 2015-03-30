859266cd4d5be0c22ef157c79d2696f8a90804aa
Andraz Vrhovec <andraz@koofr.net>
2015-03-30 14:08:05 +0200

    Initial commit

---
+++ koofr/client.py
@@ -0,0 +1,79 @@
+import requests
+import posixpath
+import os
+import json
+
+class KoofrClient:
+
+    def __init__(self, api_base = "https://stage.koofr.net", verify_ssl = True):
+        self.api_base = api_base
+        self.S = requests.Session()
+
+    def set_token(self, token):
+        self._token = token
+        self.S.headers['Authorization'] = 'Token ' + self._token
+
+    def authenticate(self, email, password):
+        resp = self.S.get(self.api_base + '/token', headers = {
+            'X-Koofr-Email': email,
+            'X-Koofr-Password': password
+        })
+
+        resp.raise_for_status()
+        self.set_token(resp.headers['X-Koofr-Token'])
+        return True
+
+    def files_get(self, mount_id, path):
+        url = self.api_base + "/content/api/v2/mounts/" + mount_id + "/files/get"
+        params = {'path': path}
+        resp = self.S.get(url, params = params)
+        resp.raise_for_status()
+        return resp.content
+
+    def files_get_to_path(self, mount_id, remote_path, local_path):
+        url = self.api_base + "/content/api/v2/mounts/" + mount_id + "/files/get"
+        params = {'path': remote_path}
+        resp = self.S.get(url, params = params)
+
+        with open(local_path, 'wb') as f:
+            for chunk in resp.iter_content(chunk_size=1024):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+            f.flush()
+            os.fsync(f)
+
+    def files_put(self, mount_id, path, content):
+        url = self.api_base + "/content/api/v2/mounts/" + mount_id + "/files/put"
+        params = {'path': posixpath.dirname(path)}
+        f = {'file': (posixpath.basename(path), content)}
+        resp = self.S.post(url, params = params, files = f)
+        resp.raise_for_status()
+        return resp.json()[0]['name']
+
+    def files_list(self, mount_id, path):
+        url = self.api_base + "/api/v2/mounts/" + mount_id + "/files/list"
+        params = {'path': path}
+        resp = self.S.get(url, params = params)
+        resp.raise_for_status()
+        return resp.json()["files"]
+
+    def files_info(self, mount_id, path):
+        url = self.api_base + "/api/v2/mounts/" + mount_id + "/files/info"
+        params = {'path': path}
+        resp = self.S.get(url, params = params)
+        return resp.json()
+
+    def files_new_folder(self, mount_id, path):
+        url = self.api_base + "/api/v2/mounts/" + mount_id + "/files/folder"
+        params = {'path': posixpath.dirname(path)}
+        data = {'name': posixpath.basename(path)}
+        resp = self.S.post(url, params = params, data = json.dumps(data), headers = {'Content-Type': 'application/json'})
+        resp.raise_for_status()
+        return True
+
+    def files_remove(self, mount_id, path):
+        url = self.api_base + "/api/v2/mounts/" + mount_id + "/files/remove"
+        params = {'path': path}
+        resp = self.S.delete(url, params = params)
+        resp.raise_for_status()
+        return True
---
+++ koofr/test_client.py
@@ -0,0 +1,54 @@
+import pytest
+import koofr
+import posixpath
+import os
+import time
+
+testdir = "/koofr-python-test" + str(time.time())
+
+@pytest.fixture()
+def mount_id():
+    assert "KOOFR_MOUNTID" in os.environ
+    return os.environ["KOOFR_MOUNTID"]
+
+@pytest.fixture()
+def client():
+    assert "KOOFR_USERNAME" in os.environ
+    assert "KOOFR_PASSWORD" in os.environ
+
+    try:
+        client = koofr.client.KoofrClient(os.environ["KOOFR_API_BASE"])
+    except:
+        client = koofr.client.KoofrClient()
+
+    ret = client.authenticate(os.environ["KOOFR_USERNAME"], os.environ["KOOFR_PASSWORD"])
+    assert ret == True
+    return client
+
+def test_new_folder(client, mount_id):
+    success = client.files_new_folder(mount_id, posixpath.join(testdir))
+    assert success
+
+def test_put(client, mount_id):
+    name = client.files_put(mount_id, posixpath.join(testdir, "test1234.txt"), "test")
+    assert name == "test1234.txt"
+
+def test_info(client, mount_id):
+    info = client.files_info(mount_id, posixpath.join(testdir, "test1234.txt"))
+    assert info["name"] == "test1234.txt"
+
+def test_get(client, mount_id):
+    content = client.files_get(mount_id, posixpath.join(testdir, "test1234.txt"))
+    assert content == "test"
+
+def test_list(client, mount_id):
+    list = client.files_list(mount_id, posixpath.join(testdir))
+    exist = False
+    for f in list:
+        if f["name"] == "test1234.txt":
+            exist = True
+    assert exist == True
+
+def test_remove(client, mount_id):
+    success = client.files_remove(mount_id, posixpath.join(testdir, "test1234.txt"))
+    assert success == True
